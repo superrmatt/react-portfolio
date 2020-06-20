6bfbfdaeb9450b16a39c614294b9e5fa365307a0
Matt Dambra <mpdambra@gmail.com>
2020-06-20 10:11:41 -0400

    fixing message form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,6 +6,7 @@ function Form() {
     <form
       action="mailto:mpdambra@gmail.com"
       method="post"
+      type="text/plain"
     >
       Name:<br></br>
       <input type="text" className="name" name="name" />
